552db48b059415ebe639239e9b51c9c235216117
EchoZoneOfficial <alexseimed81@mail.ru>
2026-04-16 16:51:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ function chooseFaction(faction) {
 
   document.getElementById("factionResult").innerText = text;
 }
+// Переход на страницу фракции
+function goFaction(faction) {
+  window.location.href = "faction.html?f=" + faction;
+}
+
+// Звук атмосферы
+const audio = new Audio("https://actions.google.com/sounds/v1/alarms/industrial_alarm.ogg");
+
+function playSound() {
+  audio.volume = 0.2;
+  audio.play();
+}
